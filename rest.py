518a52046f80faf36e0f68eeb61ff2dd2bcc0181
Sreejith N <jithupreethy@gmail.com>
2020-08-25 20:48:24 +0530

    first commit

---
+++ rest.py
@@ -0,0 +1,27 @@
+from flask import Flask 
+from flask_restful import Api , Resource , reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name" , type=str , help="Name of video" , required=True)
+video_put_args.add_argument("views" , type=str , help="Views of video" , required=True)
+video_put_args.add_argument("likes" , type=str , help="Likes of video" , required=True)
+
+videos = {}
+
+class Video(Resource):
+    def get(self,video_id):
+        return videos[video_id]
+
+    def put(self , video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id] , 201
+
+
+api.add_resource(Video , "/video/<int:video_id>")
+
+if __name__ == "__main__":
+    app.run(debug = True)
